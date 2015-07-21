c6827d8c327b14971a9850898bf6a9d77a17234e
ricrui3 <ricrui3@gmail.com>
2015-07-21 13:07:17 -0500

    Comments and readability

--- src/Seleccion/Seleccion.java
+++ src/Seleccion/Seleccion.java
@@ -6,25 +6,38 @@ import javax.swing.JFileChooser;
 public class Seleccion{
     
     public static File[] Seleccion(){
+        
         int count  = 0;
+        int resultado;
+        
+        //Seleccion multiple de arcivhos--------------
         JFileChooser fileChooser = new JFileChooser();
         fileChooser.setMultiSelectionEnabled(true);
-        int resultado = fileChooser.showOpenDialog(null);
+        //--------------------------------------------
+        
+        resultado = fileChooser.showOpenDialog(null);
+        
         if(resultado==JFileChooser.APPROVE_OPTION){
+            
             File[] f = fileChooser.getSelectedFiles();
             System.out.println("No. archivos seleccionados: "+ f.length);
+            
             for(count = 0;count < f.length;count++){
+                
                 if(f[count].length() < 1024)//bytes
-                    System.out.println("\n" + (count+1) +".- Archivo seleccionado: "+ f[count] + " " + f[count].length()+ " bytessss");
+                    System.out.println("\n" + (count + 1) + ".- Archivo seleccionado: "+ f[count] + " " + f[count].length()+ " bytessss");
                 else
                     if(f[count].length()/1024 < 1024)//kbytes
                         System.out.println("\n" + (count+1) +".- Archivo seleccionado: "+ f[count] + " " + f[count].length()/1024 + " kbytessss");
                     else//Mbytes
                         System.out.println("\n" + (count+1) +".- Archivo seleccionado: "+ f[count] + " " + (f[count].length()/1024)/1024 + " Mbytesss");
             }
+            
             return f;
         }
+        
         return null;
+        
     }//constructor
 
 }
--- src/servidor/Servidor.java
+++ src/servidor/Servidor.java
@@ -7,7 +7,8 @@ public class Servidor {
 	public static void main(String[] args) throws Exception{
 		int count;
                 File[] f;
-                
+               
+                //Instauracion de el socket
 		Socket cl = new Socket(InetAddress.getByName("127.0.0.1"),5001);
 		System.out.println("Servidor conectado..\n recibiendo nombres..");
                 
@@ -17,21 +18,28 @@ public class Servidor {
                 ois.close();
                 //-----------------------------------------------------------------
                 
-                System.out.println("No de archivos: "+f.length);
+                System.out.println("No de archivos: "+ f.length);
                 
 		//
                 for(count = 0; count < f.length; count++){
-                    cl = new Socket(InetAddress.getByName("127.0.0.1"),5001);
+                    
+                    cl = new Socket( InetAddress.getByName( "127.0.0.1" ) , 5001 );
                     System.out.println("Servidor conectado.. recibiendo archivo no: "+ count +"\nNombre del Archivo: "+f[count].getName());
+                    
                     BufferedInputStream bis = new BufferedInputStream(cl.getInputStream());
                     BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(f[count].getName()));
+                    
                     //cl.setSoTimeout(3000);
+                    
                     byte[] buf = new byte[1024];
                     int leidos;
-                    while((leidos=bis.read(buf,0,buf.length))!=-1){
+                    while((leidos = bis.read(buf,0,buf.length))!=-1){
+                        
                             bos.write(buf,0,leidos);
                             bos.flush();
+                            
                     }//while
+                    
                     System.out.println("Archivo copiado....");
                     bis.close();
                     bos.close();
